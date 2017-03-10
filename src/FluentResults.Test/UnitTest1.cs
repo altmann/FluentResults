ac3b57cd8812e56e11f4206a208c37e4f359a9eb
altmann <mic.altmann@gmail.com>
2017-03-10 13:22:55 +0100

    Add testproject

---
+++ src/FluentResults.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace FluentResults.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
