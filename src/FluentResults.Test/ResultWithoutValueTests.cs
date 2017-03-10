1f92b73c102bff071827939253a3522835c14a86
altmann <mic.altmann@gmail.com>
2017-03-10 20:27:14 +0100

    Add first test

---
+++ src/FluentResults.Test/ResultWithoutValueTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FluentResults.Test
+{
+    public class ResultWithoutValueTests
+    {
+        [Fact]
+        public void OkResult_EmptyReasons()
+        {
+            // Act
+            var okResult = Results.Ok();
+
+            // Assert
+            okResult.Reasons
+                .Should().BeEmpty();
+        }
+    }
+}
