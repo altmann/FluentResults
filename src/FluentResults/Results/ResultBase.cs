464d24e5f1d7860700bfbd9f8921f478a07e1f7a
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2017-07-19 21:18:01 +0200

    Add HttpStatusCode on ResultBase

--- src/FluentResults/Results/ResultBase.cs
+++ src/FluentResults/Results/ResultBase.cs
@@ -1,103 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace FluentResults
 {
-    public abstract class ResultBase
-    {
-        public bool IsFailed => Reasons.OfType<Error>().Any();
-        public bool IsSuccess => !IsFailed;
+	public abstract class ResultBase
+	{
+		public bool IsFailed => Reasons.OfType<Error>().Any();
+		public bool IsSuccess => !IsFailed;
 
-        public List<Reason> Reasons { get; }
-        public List<Error> Errors => Reasons.OfType<Error>().ToList();
-        public List<Success> Successes => Reasons.OfType<Success>().ToList();
+		public List<Reason> Reasons { get; }
+		public List<Error> Errors => Reasons.OfType<Error>().ToList();
+		public List<Success> Successes => Reasons.OfType<Success>().ToList();
+		public HttpStatusCode HttpStatusCode { get; set; }
 
-        protected ResultBase()
-        {
-            Reasons = new List<Reason>();
-        }
-    }
+		protected ResultBase()
+		{
+			Reasons = new List<Reason>();
+			HttpStatusCode = HttpStatusCode.OK;
+		}
+	}
 
-    public abstract class ResultBase<TResult> : ResultBase
-        where TResult : ResultBase<TResult>
-    {
-        public TResult WithReason(Reason reason)
-        {
-            Reasons.Add(reason);
-            return (TResult)this;
-        }
+	public abstract class ResultBase<TResult> : ResultBase
+		where TResult : ResultBase<TResult>
+	{
+		public TResult WithReason(Reason reason)
+		{
+			Reasons.Add(reason);
+			return (TResult) this;
+		}
 
-        public TResult WithError(string errorMessage)
-        {
-            return WithError(new Error(errorMessage));
-        }
+		public TResult WithError(string errorMessage)
+		{
+			return WithError(new Error(errorMessage));
+		}
 
-        public TResult WithError(Error error)
-        {
-            return WithReason(error);
-        }
+		public TResult WithError(Error error)
+		{
+			return WithReason(error);
+		}
 
-        public TResult WithError<TError>()
-            where TError : Error, new()
-        {
-            return WithError(new TError());
-        }
+		public TResult WithError<TError>()
+			where TError : Error, new()
+		{
+			return WithError(new TError());
+		}
 
-        public TResult WithSuccess(string successMessage)
-        {
-            return WithSuccess(new Success(successMessage));
-        }
+		public TResult WithSuccess(string successMessage)
+		{
+			return WithSuccess(new Success(successMessage));
+		}
 
-        public TResult WithSuccess(Success success)
-        {
-            return WithReason(success);
-        }
+		public TResult WithSuccess(Success success)
+		{
+			return WithReason(success);
+		}
 
-        public TResult WithSuccess<TSuccess>()
-            where TSuccess : Success, new()
-        {
-            return WithSuccess(new TSuccess());
-        }
-        
-        public TResult With(Action<TResult> setProperty)
-        {
-            setProperty((TResult)this);
-            return (TResult)this;
-        }
+		public TResult WithSuccess<TSuccess>()
+			where TSuccess : Success, new()
+		{
+			return WithSuccess(new TSuccess());
+		}
 
-        public Result<TNewValue> ConvertToResultWithValueType<TNewValue>()
-        {
-            return ResultHelper.Merge<Result<TNewValue>>(this);
-        }
+		public TResult WithHttpStatusCode(HttpStatusCode httpStatusCode)
+		{
+			HttpStatusCode = httpStatusCode;
+			return (TResult) this;
+		}
 
-        public TNewResult ConvertToResultOfType<TNewResult>()
-            where TNewResult : ResultBase<TNewResult>, new()
-        {
-            return ResultHelper.Merge<TNewResult>(this);
-        }
+		public TResult With(Action<TResult> setProperty)
+		{
+			setProperty((TResult) this);
+			return (TResult) this;
+		}
 
-        public TResult Log()
-        {
-            return Log(string.Empty);
-        }
+		public Result<TNewValue> ConvertToResultWithValueType<TNewValue>()
+		{
+			return ResultHelper.Merge<Result<TNewValue>>(this);
+		}
 
-        public TResult Log(string context)
-        {
-            var logger = Results.Settings.Logger;
+		public TNewResult ConvertToResultOfType<TNewResult>()
+			where TNewResult : ResultBase<TNewResult>, new()
+		{
+			return ResultHelper.Merge<TNewResult>(this);
+		}
 
-            logger.Log(context, this);
+		public TResult Log()
+		{
+			return Log(string.Empty);
+		}
 
-            return (TResult)this;
-        }
+		public TResult Log(string context)
+		{
+			var logger = Results.Settings.Logger;
 
-        public override string ToString()
-        {
-            var reasonsString = Reasons.Any()
-                ? $", Reasons='{ReasonFormat.ReasonsToString(Reasons)}'"
-                : string.Empty;
+			logger.Log(context, this);
 
-            return $"Result: IsSuccess='{IsSuccess}'{reasonsString}";
-        }
-    }
+			return (TResult) this;
+		}
+
+		public override string ToString()
+		{
+			var reasonsString = Reasons.Any()
+				? $", Reasons='{ReasonFormat.ReasonsToString(Reasons)}'"
+				: string.Empty;
+
+			return $"Result: IsSuccess='{IsSuccess}'{reasonsString}";
+		}
+	}
 }
