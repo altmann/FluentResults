d012e0cb26ac15eb1a12eebf078b3c468ddceb87
altmann <mic.altmann@gmail.com>
2018-02-15 20:02:03 +0100

    Rename tests

--- src/FluentResults.Test/ResultWithValueTests.cs
+++ src/FluentResults.Test/ResultWithValueTests.cs
@@ -8,7 +8,7 @@ namespace FluentResults.Test
     public class ResultWithValueTests
     {
         [TestMethod]
-        public void CreateOkResult_SuccessResult()
+        public void Ok_WithNoParams_ShouldReturnSuccessResult()
         {
             // Act
             var okResult = Results.Ok<int>();
@@ -21,41 +21,47 @@ namespace FluentResults.Test
             okResult.Errors.Should().BeEmpty();
             okResult.Successes.Should().BeEmpty();
             okResult.Value.Should().Be(0);
+            okResult.ValueOrDefault.Should().Be(0);
         }
 
         [TestMethod]
-        public void CreateOkResultWithValue_SuccessResultWithValue()
+        public void Ok_WithValidValue_ShouldReturnSuccessResult()
         {
             // Act
             var okResult = Results.Ok(5);
 
             // Assert
+            okResult.IsSuccess.Should().BeTrue();
             okResult.Value.Should().Be(5);
+            okResult.ValueOrDefault.Should().Be(5);
         }
 
         [TestMethod]
-        public void CreateOkResultWithValue2_SuccessResultWithValue()
+        public void WithValue_WithValidParam_ShouldReturnSuccessResult()
         {
+            var okResult = Results.Ok<int>();
+
             // Act
-            var okResult = Results.Ok<int>()
-                .WithValue(5);
+            okResult.WithValue(5);
 
             // Assert
             okResult.Value.Should().Be(5);
+            okResult.ValueOrDefault.Should().Be(5);
         }
 
         [TestMethod]
-        public void CreateFailedResultWithNoExplicitValue_FailedResult()
+        public void Fail_WithValidErrorMessage_ShouldReturnFailedResult()
         {
             // Act
             var result = Results.Fail<int>("First error message");
 
             // Assert
+            result.IsFailed.Should().BeTrue();
             result.ValueOrDefault.Should().Be(0);
         }
 
         [TestMethod]
-        public void CreateFailedResult_FailedResult()
+        public void Value_WithResultInFailedState_ShouldThrowException()
         {
             // Act
             var result = Results.Fail<int>("First error message");
@@ -70,7 +76,7 @@ namespace FluentResults.Test
         }
 
         [TestMethod]
-        public void CreateFailedResultWithExplicitValue_ShouldThrowException()
+        public void WithValue_WithResultInFailedState_ShouldThrowException()
         {
             var failedResult = Results.Fail<int>("First error message");
 
