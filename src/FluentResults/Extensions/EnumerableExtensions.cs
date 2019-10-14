f4f06d55f3e1648fcef4f7ce82c802eb19e8559c
Michael Altmann <mic.altmann@gmail.com>
2019-10-14 20:10:29 +0200

    Add Merge() extension for IEnumerable

---
+++ src/FluentResults/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentResults
+{
+    public static class EnumerableExtensions
+    {
+        public static Result Merge(this IEnumerable<Result> results)
+        {
+            return ResultHelper.Merge<Result>(results.ToArray());
+        }
+
+        public static Result Merge<TValue>(this IEnumerable<Result<TValue>> results)
+        {
+            return ResultHelper.Merge<Result<TValue>>(results.ToArray());
+        }
+    }
+}
