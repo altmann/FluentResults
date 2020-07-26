a718268d388f69658038ecde002a11e275cf6304
Michael Altmann <mic.altmann@gmail.com>
2020-07-26 15:23:19 +0200

    Add sample CustomerController with ResultDto

---
+++ src/FluentResults.Samples/WebController/ResultDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace FluentResults.Samples.WebController
+{
+    public class ResultDto
+    {
+        public bool IsSuccess { get; set; }
+
+        public IEnumerable<ErrorDto> Errors { get; set; }
+
+        public ResultDto(bool isSuccess, IEnumerable<ErrorDto> errors)
+        {
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+    }
+
+    public class ErrorDto
+    {
+        public string Message { get; set; }
+
+        public string Code { get; set; }
+
+        public ErrorDto(string message, string code)
+        {
+            Message = message;
+            Code = code;
+        }
+    }
+}
---
+++ src/FluentResults.Samples/WebController/ResultDtoExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentResults.Samples.WebController
+{
+    public static class ResultDtoExtensions
+    {
+        public static ResultDto ToResultDto(this Result result)
+        {
+            if (result.IsSuccess)
+                return new ResultDto(true, Enumerable.Empty<ErrorDto>());
+
+            return new ResultDto(false, TransformErrors(result.Errors));
+        }
+
+        private static IEnumerable<ErrorDto> TransformErrors(List<Error> errors)
+        {
+            return errors.Select(TransformError);
+        }
+
+        private static ErrorDto TransformError(Error error)
+        {
+            var errorCode = TransformErrorCode(error);
+
+            return new ErrorDto(error.Message, errorCode);
+        }
+
+        private static string TransformErrorCode(Error error)
+        {
+            if (error.Metadata.TryGetValue("ErrorCode", out var errorCode))
+                return errorCode as string;
+
+            return "";
+        }
+    }
+}
