6db52de210268aa540d7f8602930a1fb5811b6ea
Michael Altmann <mic.altmann@gmail.com>
2021-10-14 20:33:50 +0200

    Remove Obsolete things

--- src/FluentResults/Factories/Results.cs
+++ src/FluentResults/Factories/Results.cs
@@ -5,65 +5,6 @@ using System.Threading.Tasks;
 // ReSharper disable once CheckNamespace
 namespace FluentResults
 {
-    [Obsolete("Removed in next major version. Use the class Result instead.")]
-    public static class Results
-    {
-        public static void Setup(Action<ResultSettingsBuilder> setupFunc)
-        {
-            Result.Setup(setupFunc);
-        }
-
-        public static Result Ok()
-        {
-            return new Result();
-        }
-
-        public static Result Fail(IError error)
-        {
-            var result = new Result();
-            result.WithError(error);
-            return result;
-        }
-
-        public static Result Fail(string errorMessage)
-        {
-            var result = new Result();
-            result.WithError(new Error(errorMessage));
-            return result;
-        }
-        
-        public static Result<TValue> Ok<TValue>(TValue value)
-        {
-            var result = new Result<TValue>();
-            result.WithValue(value);
-            return result;
-        }
-
-        public static Result<TValue> Fail<TValue>(IError error)
-        {
-            var result = new Result<TValue>();
-            result.WithError(error);
-            return result;
-        }
-
-        public static Result<TValue> Fail<TValue>(string errorMessage)
-        {
-            var result = new Result<TValue>();
-            result.WithError(new Error(errorMessage));
-            return result;
-        }
-
-        public static Result Merge(params ResultBase[] results)
-        {
-            return ResultHelper.Merge(results);
-        }
-
-        public static Result<IEnumerable<TValue>> Merge<TValue>(params Result<TValue>[] results)
-        {
-            return ResultHelper.MergeWithValue(results);
-        }
-    }
-
     public partial class Result
     {
         internal static ResultSettings Settings { get; private set; }
