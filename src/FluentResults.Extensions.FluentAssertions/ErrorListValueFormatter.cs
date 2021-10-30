41587720fc70498855d97dce33d7ce718d6c1c14
Michael Altmann <mic.altmann@gmail.com>
2021-10-30 16:59:25 +0200

    Fix: Formating list of errors

--- src/FluentResults.Extensions.FluentAssertions/ErrorListValueFormatter.cs
+++ src/FluentResults.Extensions.FluentAssertions/ErrorListValueFormatter.cs
@@ -8,12 +8,12 @@ namespace FluentResults.Extensions.FluentAssertions
     {
         public bool CanHandle(object value)
         {
-            return value is List<Error>;
+            return value is List<IError>;
         }
 
         public void Format(object value, FormattedObjectGraph formattedGraph, FormattingContext context, FormatChild formatChild)
         {
-            var errors = (IEnumerable<Error>)value;
+            var errors = (IEnumerable<IError>)value;
             formattedGraph.AddFragment(string.Join("; ", errors.Select(error => error.Message)));
         }
     }
