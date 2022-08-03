1bb23d9c9fe7e6ae6bc74454072afb8c58421880
Michael Altmann <mic.altmann@gmail.com>
2022-08-03 16:46:49 +0200

    Add async/await for MapErrors/MapSuccesses

---
+++ src/FluentResults/Extensions/ResultExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FluentResults.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static async Task<Result> MapErrors(this Task<Result> resultTask, Func<IError, IError> errorMapper)
+        {
+            var result = await resultTask;
+            return result.MapErrors(errorMapper);
+        }
+
+        public static async Task<Result<T>> MapErrors<T>(this Task<Result<T>> resultTask, Func<IError, IError> errorMapper)
+        {
+            var result = await resultTask;
+            return result.MapErrors(errorMapper);
+        }
+
+        public static async Task<Result> MapSuccesses(this Task<Result> resultTask, Func<ISuccess, ISuccess> errorMapper)
+        {
+            var result = await resultTask;
+            return result.MapSuccesses(errorMapper);
+        }
+
+        public static async Task<Result<T>> MapSuccesses<T>(this Task<Result<T>> resultTask, Func<ISuccess, ISuccess> errorMapper)
+        {
+            var result = await resultTask;
+            return result.MapSuccesses(errorMapper);
+        }
+    }
+}
