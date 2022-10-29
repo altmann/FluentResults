32851c28f9ec7286a6512b452a42d008d47dfe1b
Richard Haughton <R.Haughton86@gmail.com>
2022-10-29 18:31:52 +0200

    Added Unit Testing for Error and List<Error> Implicit conversion

--- src/FluentResults.Test/ErrorTests.cs
+++ src/FluentResults.Test/ErrorTests.cs
@@ -1,6 +1,8 @@
 ﻿using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using Xunit;
 
 namespace FluentResults.Test
@@ -121,9 +123,36 @@ namespace FluentResults.Test
             error.Metadata.Keys.Should().BeEmpty();
         }
 
-        public class CustomError : Error
-        {
+        public class CustomError : Error { }
 
+        [Fact]
+        public void CreateError_FromErrorImplicitConversion()
+        {
+            var error = new Error("")
+                .CausedBy("First error message");
+
+            Result result = error;
+
+            result.IsFailed.Should().Be(true);
+            result.Reasons.Should().HaveCount(1);
+            error.Reasons.First().Message.Should().Be("First error message");
+        }
+
+        [Fact]
+        public void CreateErrors_FromListOfErrorsImplicitConversion()
+        {
+            var errors = new List<Error>
+            {
+                new Error("First error message"),
+                new Error("Second error message"),
+            };
+
+            Result result = errors;
+
+            result.IsFailed.Should().Be(true);
+            result.Reasons.Should().HaveCount(2);
+            result.Reasons[0].Message.Should().Be("First error message");
+            result.Reasons[1].Message.Should().Be("Second error message");
         }
     }
 }
