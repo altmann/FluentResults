9da630e9a24a845aecebff518906595065ead6d4
DrPepperBianco <11694093+DrPepperBianco@users.noreply.github.com>
2023-08-23 21:48:13 +0200

    IResultTExtensions and refactor of ResultExtensions
    ResultExtensions only contains extensions for
    Task<…> and Value<…> objects.
    
    All Extensions for IResult<TValue> are now in
    IResultTExtensions.
    
    IResultTExtensions is directly in Namespace
    FluentResults.
    
    New method "MapReasons()". Makes it easier
    to implement MapErros and MapSuccesses.
    
    The overloads of Bind implemented by one method
    that is called from the other overloads. There
    are two variants. Bind() that returns Result without
    value and Bind() that returns Result with value.
    
    Return value is always directly Result or
    Result<TValue> except for "MapErrors", because
    MapErrors can return the IResult<TValue> argument
    directly. All other methods always create a new
    object as return value.

---
+++ src/FluentResults/Extensions/IResultTExtensions.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace FluentResults
+{
+    /// <summary>
+    /// Extensions methods for <see cref="IResult{TValue}"/>
+    /// </summary>
+    public static class IResultTExtensions
+    {
+        /// <summary>
+        /// Map all reasons of the result via reasonMapper
+        /// </summary>
+        /// <remarks>
+        /// If TReason is different from IReason the mapper is only to applied to 
+        /// reasons of the appropriate type.
+        /// </remarks>
+        public static Result<TValue> MapReasons<TValue, TReason>(this IResult<TValue> result, Func<TReason, TReason> reasonMapper)
+            where TReason : IReason =>
+            // Create new Result object
+            new Result<TValue>()
+            // Set value, if any
+            .WithValue(result.ValueOrDefault)
+            // Set and map all reasons
+            .WithReasons(
+                result.Reasons.Select(reason =>
+                    reason is TReason tReason
+                    ? reasonMapper(tReason)
+                    : reason));
+
+
+        /// <inheritdoc cref="Result{TValue}.MapErrors(Func{IError, IError})"/>
+        public static IResult<TValue> MapErrors<TValue>(this IResult<TValue> result, Func<IError, IError> errorMapper) =>
+            result.IsSuccess
+                ? result
+                : result.MapReasons(errorMapper);
+
+        /// <inheritdoc cref="Result{TValue}.MapSuccesses(Func{ISuccess, ISuccess})"/>
+        public static Result<TValue> MapSuccesses<TValue>(this IResult<TValue> result, Func<ISuccess, ISuccess> successMapper) =>
+            result.MapReasons(successMapper);
+
+        /// <inheritdoc cref="Result{TValue}.ToResult()"/>
+        public static Result ToResult<TValue>(this IResult<TValue> result) =>
+            new Result()
+                .WithReasons(result.Reasons);
+
+        /// <inheritdoc cref="Result{TValue}.ToResult{TNewValue}(Func{TValue, TNewValue})"/>
+        public static Result<TNewValue> ToResult<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, TNewValue> valueConverter = null) =>
+            Map(result, valueConverter);
+
+        /// <inheritdoc cref="Result{TValue}.Map{TNewValue}(Func{TValue, TNewValue})"/>
+        public static Result<TNewValue> Map<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, TNewValue> mapLogic)
+        {
+            if(result.IsSuccess && mapLogic == null)
+                throw new ArgumentException("If result is success then valueConverter should not be null");
+
+            return new Result<TNewValue>()
+                   .WithValue(result.IsFailed ? default : mapLogic(result.Value))
+                   .WithReasons(result.Reasons);
+        }
+
+
+        #region Variants of Bind(), that return result with value
+
+        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, Result{TNewValue}})"/>
+        public static Result<TNewValue> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, IResult<TNewValue>> bind) =>
+            Bind(result, new Func<TValue, ValueTask<IResult<TNewValue>>>(value => new ValueTask<IResult<TNewValue>>(bind(value))))
+            .GetAwaiter().GetResult();
+
+        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, Task{Result{TNewValue}}})"/>
+        public static Task<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, Task<Result<TNewValue>>> bind) =>
+            Bind(result, new Func<TValue, Task<IResult<TNewValue>>>(async (value) => await bind(value)));
+
+        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, Task{Result{TNewValue}}})"/>
+        public static Task<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, Task<IResult<TNewValue>>> bind) =>
+            Bind(result, new Func<TValue, ValueTask<IResult<TNewValue>>>(async (value) => await bind(value)))
+            .AsTask();
+        
+        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, ValueTask{Result{TNewValue}}})"/>
+        public static ValueTask<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, ValueTask<Result<TNewValue>>> bind) =>
+            Bind(result, new Func<TValue, ValueTask<IResult<TNewValue>>>(async (value) => await bind(value)));
+
+        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, ValueTask{Result{TNewValue}}})"/>
+        public static async ValueTask<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, ValueTask<IResult<TNewValue>>> bind)
+        {
+            var outResult = new Result<TNewValue>();
+            outResult.WithReasons(result.Reasons);
+
+            if(result.IsSuccess)
+            {
+                var converted = await bind(result.Value);
+                outResult.WithValue(converted.ValueOrDefault);
+                outResult.WithReasons(converted.Reasons);
+            }
+
+            return outResult;
+        }
+
+        #endregion
+
+
+        #region Variants of Bind(), that return result without value
+
+        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, Result})"/>
+        public static Result Bind<TValue>(this IResult<TValue> result, Func<TValue, IResultBase> action) =>
+            Bind(result, new Func<TValue, ValueTask<IResultBase>>(value => new ValueTask<IResultBase>(action(value))))
+            .GetAwaiter().GetResult();
+
+        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, Task{Result}})"/>
+        public static Task<Result> Bind<TValue>(this IResult<TValue> result, Func<TValue, Task<Result>> action) =>
+            Bind(result, new Func<TValue, Task<IResultBase>>(async (value) => await action(value)));
+
+        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, Task{Result}})"/>
+        public static Task<Result> Bind<TValue>(this IResult<TValue> result, Func<TValue, Task<IResultBase>> action) =>
+            Bind(result, new Func<TValue, ValueTask<IResultBase>>(async (value) => await action(value)))
+            .AsTask();
+
+        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, ValueTask{Result}})"/>
+        public static ValueTask<Result> Bind<TValue>(this IResult<TValue> result, Func<TValue, ValueTask<Result>> action) =>
+            Bind(result, new Func<TValue, ValueTask<IResultBase>>(async (value) => await action(value)));
+
+        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, ValueTask{Result}})"/>
+        public static async ValueTask<Result> Bind<TValue>(this IResult<TValue> result, Func<TValue, ValueTask<IResultBase>> action)
+        {
+            var outResult = new Result();
+            outResult.WithReasons(result.Reasons);
+
+            if(result.IsSuccess)
+            {
+                var converted = await action(result.Value);
+                outResult.WithReasons(converted.Reasons);
+            }
+
+            return outResult;
+        }
+
+        #endregion
+    }
+}
--- src/FluentResults/Extensions/ResultExtensions.cs
+++ src/FluentResults/Extensions/ResultExtensions.cs
@@ -9,6 +9,8 @@ namespace FluentResults.Extensions
     /// </summary>
     public static class ResultExtensions
     {
+        #region Extensions for Task<Result>, ValueTask<Result>, Task<Result<T>>, and ValueTask<Result<T>>
+
         /// <summary>
         /// Map all errors of the result via errorMapper
         /// </summary>
@@ -331,8 +333,12 @@ namespace FluentResults.Extensions
             return result.ToResult(value);
         }
 
-        #region Interfaces
+        #endregion
 
+
+#region Extensions for Task<IResultBase>, ValueTask<IResultBase>, Task<IResult<T>>, and ValueTask<IResult<T>>
+
+#if false
         public static async Task<IResultBase> MapErrors(this Task<IResultBase> resultTask, Func<IError, IError> errorMapper)
         {
             var result = await resultTask;
@@ -344,6 +350,7 @@ namespace FluentResults.Extensions
             var result = await resultTask;
             return result.MapErrors(errorMapper);
         }
+#endif
 
         public static async Task<IResult<T>> MapErrors<T>(this Task<IResult<T>> resultTask, Func<IError, IError> errorMapper)
         {
@@ -357,6 +364,7 @@ namespace FluentResults.Extensions
             return result.MapErrors(errorMapper);
         }
 
+#if false
         public static async Task<IResultBase> MapSuccesses(this Task<IResultBase> resultTask, Func<ISuccess, ISuccess> errorMapper)
         {
             var result = await resultTask;
@@ -368,6 +376,7 @@ namespace FluentResults.Extensions
             var result = await resultTask;
             return result.MapSuccesses(errorMapper);
         }
+#endif
 
         public static async Task<IResult<T>> MapSuccesses<T>(this Task<IResult<T>> resultTask, Func<ISuccess, ISuccess> errorMapper)
         {
@@ -429,6 +438,7 @@ namespace FluentResults.Extensions
             return await result.Bind(bind);
         }
 
+#if false
         public static async Task<IResult<TNew>> Bind<TNew>(this Task<IResultBase> resultTask, Func<Task<IResult<TNew>>> bind)
         {
             var result = await resultTask;
@@ -452,6 +462,7 @@ namespace FluentResults.Extensions
             var result = await resultTask;
             return await result.Bind(bind);
         }
+#endif
 
         public static async Task<IResult<TNewValue>> Map<TOldValue, TNewValue>(this Task<IResult<TOldValue>> resultTask, Func<TOldValue, TNewValue> valueConverter)
         {
@@ -465,23 +476,45 @@ namespace FluentResults.Extensions
             return result.Map(valueConverter);
         }
 
-        public static async Task<IResult<TValue>> ToResult<TValue>(this Task<IResultBase> resultTask, TValue value)
+        #endregion
+
+        #region Convert (Value)Task of IResultBase into Task of Result
+
+        public static async Task<Result> ToResult(this Task<IResultBase> resultTask)
         {
             var result = await resultTask;
-            return result.ToResult(value);
+            return new Result()
+                .WithReasons(result.Reasons);
         }
 
-        public static async Task<IResult<TValue>> ToResult<TValue>(this ValueTask<IResultBase> resultTask, TValue value)
+        public static async ValueTask<Result> ToResult(this ValueTask<IResultBase> resultTask)
         {
             var result = await resultTask;
-            return result.ToResult(value);
+            return new Result()
+                .WithReasons(result.Reasons);
+        }
+
+        public static async Task<Result<TValue>> ToResult<TValue>(this Task<IResultBase> resultTask, TValue value)
+        {
+            var result = await resultTask;
+            return new Result<TValue>()
+                .WithValue(value)
+                .WithReasons(result.Reasons);
+        }
+
+        public static async ValueTask<Result<TValue>> ToResult<TValue>(this ValueTask<IResultBase> resultTask, TValue value)
+        {
+            var result = await resultTask;
+            return new Result<TValue>()
+                .WithValue(value)
+                .WithReasons(result.Reasons);
         }
 
         #endregion
 
 
         #region Erweiterung: Member von Result für Interface IResultBase
-
+#if false
         /// <inheritdoc cref="Result.MapErrors(Func{IError, IError})"/>
         public static IResultBase MapErrors(this IResultBase result, Func<IError, IError> errorMapper) =>
             result.IsSuccess 
@@ -594,149 +627,9 @@ namespace FluentResults.Extensions
 
             return out_result;
         }
-
+#endif
         #endregion
 
 
-        #region Erweiterung: Member von Result für Interface IResultBase
-
-        /// <inheritdoc cref="Result{TValue}.WithValue(TValue)"/>
-        public static IResult<TValue> WithValue<TValue>(this IResult<TValue> result, TValue value) =>
-            // ℹ️ You would assume immutability on results,
-            //     therefor we first create a copy!
-            new Result<TValue>()
-                .WithReasons(result.Reasons)
-                .WithValue(value);
-
-
-        /// <inheritdoc cref="Result{TValue}.MapErrors(Func{IError, IError})"/>
-        public static IResult<TValue> MapErrors<TValue>(this IResult<TValue> result, Func<IError, IError> errorMapper) =>
-            result.IsSuccess
-                ? result
-                : new Result<TValue>()
-                    .WithErrors(result.Errors.Select(errorMapper))
-                    .WithSuccesses(result.Successes);
-
-        /// <inheritdoc cref="Result{TValue}.MapSuccesses(Func{ISuccess, ISuccess})"/>
-        public static IResult<TValue> MapSuccesses<TValue>(this IResult<TValue> result, Func<ISuccess, ISuccess> successMapper) =>
-            new Result<TValue>()
-                .WithValue(result.ValueOrDefault)
-                .WithErrors(result.Errors)
-                .WithSuccesses(result.Successes.Select(successMapper));
-
-        /// <inheritdoc cref="Result{TValue}.ToResult()"/>
-        public static IResultBase ToResult<TValue>(this IResult<TValue> result) =>
-            new Result()
-                .WithReasons(result.Reasons);
-
-        /// <inheritdoc cref="Result{TValue}.ToResult{TNewValue}(Func{TValue, TNewValue})"/>
-        public static Result<TNewValue> ToResult<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, TNewValue> valueConverter = null) =>
-            Map(result, valueConverter);
-
-        /// <inheritdoc cref="Result{TValue}.Map{TNewValue}(Func{TValue, TNewValue})"/>
-        public static Result<TNewValue> Map<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, TNewValue> mapLogic)
-        {
-            if(result.IsSuccess && mapLogic == null)
-                throw new ArgumentException("If result is success then valueConverter should not be null");
-
-            return new Result<TNewValue>()
-                   .WithValue(result.IsFailed ? default : mapLogic(result.Value))
-                   .WithReasons(result.Reasons);
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, Result{TNewValue}})"/>
-        public static Result<TNewValue> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, IResult<TNewValue>> bind)
-        {
-            var out_result = new Result<TNewValue>();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = bind(result.Value);
-                out_result.WithValue(converted.ValueOrDefault);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, Task{Result{TNewValue}}})"/>
-        public static async Task<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, Task<IResult<TNewValue>>> bind)
-        {
-            var out_result = new Result<TNewValue>();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = await bind(result.Value);
-                out_result.WithValue(converted.ValueOrDefault);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind{TNewValue}(Func{TValue, ValueTask{Result{TNewValue}}})"/>
-        public static async ValueTask<Result<TNewValue>> Bind<TValue, TNewValue>(this IResult<TValue> result, Func<TValue, ValueTask<IResult<TNewValue>>> bind)
-        {
-            var out_result = new Result<TNewValue>();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = await bind(result.Value);
-                out_result.WithValue(converted.ValueOrDefault);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, Result})"/>
-        public static IResultBase Bind<TValue>(this IResult<TValue> result, Func<TValue, IResultBase> action)
-        {
-            var out_result = new Result();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = action(result.Value);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, Task{Result}})"/>
-        public static async Task<IResultBase> Bind<TValue>(this IResult<TValue> result, Func<TValue, Task<IResultBase>> action)
-        {
-            var out_result = new Result();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = await action(result.Value);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        /// <inheritdoc cref="Result{TValue}.Bind(Func{TValue, ValueTask{Result}})"/>
-        public static async ValueTask<IResultBase> Bind<TValue>(this IResult<TValue> result, Func<TValue, ValueTask<IResultBase>> action)
-        {
-            var out_result = new Result();
-            out_result.WithReasons(result.Reasons);
-
-            if(result.IsSuccess)
-            {
-                var converted = await action(result.Value);
-                out_result.WithReasons(converted.Reasons);
-            }
-
-            return out_result;
-        }
-
-        #endregion
     }
 }
