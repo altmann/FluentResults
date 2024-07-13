245339c5e03df3c6e0a6ae84422718cdd9e99a22
Kyle Slusser <Kysluss@aol.com>
2024-07-13 13:36:04 -0400

    Add documentation comments for ReasonStringBuilder

--- src/FluentResults/Reasons/ReasonStringBuilder.cs
+++ src/FluentResults/Reasons/ReasonStringBuilder.cs
@@ -5,17 +5,31 @@ using System.Linq;
 // ReSharper disable once CheckNamespace
 namespace FluentResults
 {
+    /// <summary>
+    /// Creates the text to dispay when serializing reason instances
+    /// </summary>
     public class ReasonStringBuilder
     {
         private string _reasonType = string.Empty;
         private readonly List<string> _infos = new List<string>();
          
+        /// <summary>
+        /// Specify the type of reason
+        /// </summary>
+        /// <param name="type">The type of reason</param>
+        /// <returns>Reference to the current Builder</returns>
         public ReasonStringBuilder WithReasonType(Type type)
         {
             _reasonType = type.Name;
             return this;
         }
 
+        /// <summary>
+        /// Add an information label for the given value
+        /// </summary>
+        /// <param name="label">The label to display</param>
+        /// <param name="value">The value the label is for</param>
+        /// <returns>A reference to the current Builder</returns>
         public ReasonStringBuilder WithInfo(string label, string value)
         {
             var infoString = value.ToLabelValueStringOrEmpty(label);
@@ -28,6 +42,10 @@ namespace FluentResults
             return this;
         }
 
+        /// <summary>
+        /// Create the reason string from the current information
+        /// </summary>
+        /// <returns>The reason string</returns>
         public string Build()
         {
             var reasonInfoText = _infos.Any()
