f9dd0a74501a6645c3a28c68ad785d256e647d0c
Kyle Slusser <Kysluss@aol.com>
2024-07-13 11:15:03 -0400

    Add documentation comments for ObjectExtensions

--- src/FluentResults/Extensions/ObjectExtensions.cs
+++ src/FluentResults/Extensions/ObjectExtensions.cs
@@ -1,8 +1,16 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace FluentResults
 {
+    /// <summary>
+    /// Extension methods for object base type
+    /// </summary>
     public static class ObjectExtensions
     {
+        /// <summary>
+        /// Convert value to result
+        /// </summary>
+        /// <typeparam name="TValue">The type of the result's <see cref="Result{TValue}.Value"/></typeparam>
+        /// <param name="value">The value of the result</param>
         public static Result<TValue> ToResult<TValue>(this TValue value)
         {
             return new Result<TValue>()
