ec1f53bf2c1205193a80c7279fbb2a364d7985f4
Kyle Slusser <Kysluss@aol.com>
2024-07-13 11:10:14 -0400

    Add documentation comments for Logging

--- src/FluentResults/Logging/DefaultLogger.cs
+++ src/FluentResults/Logging/DefaultLogger.cs
@@ -4,13 +4,18 @@ using Microsoft.Extensions.Logging;
 
 namespace FluentResults
 {
+    /// <summary>
+    /// Default implementation of <see cref="IResultLogger"/> that doesn't log anything
+    /// </summary>
     public class DefaultLogger : IResultLogger
     {
+        /// <inheritdoc/>
         public void Log(string context, string content, ResultBase result, LogLevel logLevel)
         {
 
         }
 
+        /// <inheritdoc/>
         public void Log<TContext>(string content, ResultBase result, LogLevel logLevel)
         {
 
